e7c53921b0239d27681bde65a497369d352a5939
shubhamshelke307 <shubhamshelke307@gmail.com>
2021-02-21 13:12:51 +0530

    My html code

---
+++ binary.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class binary{
+    public static void main(String args[]){
+        int n=10;
+        int a[]=new int[10];
+        int i=0;   
+        while(n!=0){
+          a[i]=n%2;
+          n=n/2;
+          i++;      
+        }
+for(int j=i-1;j>=0;j--){
+System.out.print(a[j]);
+    }
+}
+}
